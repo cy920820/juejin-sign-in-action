e44ae56a56be5d5fb741e5d4174a78c8af8871d2
cy920820 <cuiyang673308817@gmail.com>
2021-08-03 11:02:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# 掘金每日签到 + 抽奖
+
+## Usage
+
+使用 github action 每日定时功能
+
+1. Fork 本项目
+
+2. 添加 Secrets
+
+方法：Settings -> Secrets -> New repository secret
+
+- JJ_COOKIE (掘金 cookie，去到掘金页面找到任一 api 即可获取；必填)
+- SERVER_KEY ([server 酱](https://sct.ftqq.com/sendkey) key，用于将脚本结果发送 server 酱通知；选填)
+
+3. 修改 .github/workflows 定时策略 cron
+
+参考 github action 文档 [CRON](https://docs.github.com/cn/actions/reference/events-that-trigger-workflows#scheduled-events)
+
+```yml
+schedule:
+    - cron: '0 22 * * *'
+```
+
+## 免责
+
+本代码仅用于学习，下载后请勿用于商业用途，并且使用该脚本发生的一切后果与本人无关
---
+++ signin.py
@@ -0,0 +1,32 @@
+import os
+
+import requests
+
+# 添加 server 酱通知
+server_key = os.environ["SERVER_KEY"]
+
+# 获取掘金 cookie
+jj_cookie = os.environ["JJ_COOKIE"]
+
+# 掘金 api_url
+baseUrl = 'https://api.juejin.cn/'
+checkInUrl = baseUrl + 'growth_api/v1/check_in'
+lotteryUrl = baseUrl + 'growth_api/v1/lottery/draw'
+
+# user-agent
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36'}
+
+# server 酱消息推送
+def send_server(title, content):
+    url = "https://sctapi.ftqq.com/%s.send" % server_key
+    params = {'text': title, 'desp': content}
+    resp = requests.post(url, params=params)
+    print('server 酱推送状态码: %s' % resp.status_code)
+
+# 入口
+if __name__ == '__main__':
+    checkInResp = requests.post(checkInUrl, headers=headers, cookies={'Cookie': jj_cookie})
+    lotteryResp = requests.post(lotteryUrl, headers=headers, cookies={'Cookie': jj_cookie})
+    resultMsg = "掘金签到\n" + checkInResp.text + "\n抽奖结果\n" + lotteryResp.text
+    send_server('掘金签到+每日抽奖', resultMsg)
+    print('本次签到与抽奖结果信息:\n %s' % resultMsg)
